a99f01fc4f3b89169a89a5da3c117561ce9d10d8
Noumn Ahmed <noumanahmed.cs@gmail.com>
2026-07-17 11:40:46 +0500

    add user routes and controllers

---
+++ server/controllers/userController.js
@@ -0,0 +1,111 @@
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+import Resume from "../models/Resume.js";
+
+const generateToken = (userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
+  return token;
+};
+
+//POST: api/users/register
+//Controller for user registration
+export const registerUser = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    //check required field are present
+    if (!name || !email || !password) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    //check user already exist
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    //create new user
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const newUser = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    //return success message
+    const token = generateToken(newUser._id);
+    newUser.password = undefined;
+
+    return res
+      .status(201)
+      .json({ message: "User created successfully", token, user: newUser });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+//POST: api/users/login
+//Controller for user login
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    //check user exist
+    const user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    //verify password
+    if (!user.comparePassword(password)) {
+      return res.status(400).json({ message: "Invalid email or password" });
+    }
+
+    //return success message
+    const token = generateToken(user._id);
+    user.password = undefined;
+
+    return res
+      .status(201)
+      .json({ message: "User login successfully", token, user });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+//POST: api/users/data
+//Controller for getting user by id
+export const getUserById = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    //check if user exist
+    const user = await User.findById(userId);
+    if (!user) {
+      return res.status(404).json({ message: "User not found", token, user });
+    }
+
+    //return user
+    user.password = undefined;
+    return res.status(200).json({ user });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+//GET: /api/users/resumes
+//Controller for getting user resumes
+export const getUserResumes = async (req, res) => {
+  try {
+    const userId = req.userId;
+
+    //return user resumes
+    const resumes = await Resume.find({ userId });
+    return res.status(200).json({ resumes });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
---
+++ server/server.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./configs/db.js";
+import userRouter from "./routes/userRoutes.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+//DB connection
+await connectDB();
+
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Server is live...");
+});
+
+app.use("/api/users", userRouter);
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
